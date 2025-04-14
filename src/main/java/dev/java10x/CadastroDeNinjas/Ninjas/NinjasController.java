97e93748653ad6f138c8909a2661056d5c404cf9
ViniciusSotana <vinysotana@gmail.com>
2025-04-14 12:41:46 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasController.java
@@ -11,4 +11,28 @@ public class NinjasController {
             return "Essa e minha primeira mensagem nessa rota";
         }
 
+
+        // Add Ninja
+        @PostMapping("/criar")
+        public String criarNinja(){ return "ninja criado"; }
+
+
+        //Find Ninjas by ID
+        @GetMapping("/todosID")
+        public String mostrarTodosOsNinjasPorId(){ return "Mostrar ninja por ID"; }
+
+
+        //Listar Ninjas
+        @GetMapping("/todos")
+        public String mostrarTodosOsNinjas(){ return "mostrar todos os ninjas"; }
+
+
+        //Del ninja
+        @DeleteMapping("/deletarID")
+        public String deletarNinjaPorId(){ return "Deletar ninja por id"; }
+
+
+        //Update Ninja
+        @PutMapping("/alterarID")
+        public String alterarNinjaPorId(){ return "Alterar ninja por ID"; }
 }
